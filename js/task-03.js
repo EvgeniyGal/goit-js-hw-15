3df82e93898cc288acacf675f89eb6a4375e6548
Evgeniy <egeniygal@gmail.com>
2023-10-13 12:11:32 +0300

    Add FormData to the 3rd task

--- js/task-03.js
+++ js/task-03.js
@@ -1,15 +1,32 @@
 const loginForm = document.querySelector('.login-form');
 
+// function handlerCheckForm(e) {
+//   e.preventDefault();
+//   const target = e.currentTarget;
+//   const email = target.email.value;
+//   const password = target.password.value;
+
+//   if (email && password) {
+//     const user = { email, password };
+//     console.log(user);
+//     target.reset();
+//   } else {
+//     alert('You must feel both email and password!');
+//   }
+// }
+
 function handlerCheckForm(e) {
   e.preventDefault();
-  const target = e.currentTarget;
-  const email = target.email.value;
-  const password = target.password.value;
 
-  if (email && password) {
-    const user = { email, password };
+  const user = {};
+
+  new FormData(e.currentTarget).forEach((value, key) => {
+    user[key] = value;
+  });
+
+  if (user.email && user.password) {
     console.log(user);
-    target.reset();
+    e.currentTarget.reset();
   } else {
     alert('You must feel both email and password!');
   }
